48df4f4bb41926951e37d6abdfbeab0d8d756963
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-28 11:19:58 +0700

    menambahkan list lowongan

---
+++ SimponiApp/SimponiApp/Helpers/Pengaturan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimponiApp.Helpers
+{
+    public class Pengaturan
+    {
+        public static string BaseUrl = 
+            "http://api.uajy.ac.id/apisimponi/api/";
+    }
+}
---
+++ SimponiApp/SimponiApp/Services/LowonganService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using SimponiApp.Helpers;
+using SimponiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimponiApp.Services
+{
+    public class LowonganService
+    {
+        private HttpClient _client;
+        public List<Lowongan> ListLowongan { get; set; }
+        public LowonganService()
+        {
+            _client = new HttpClient();
+            _client.MaxResponseContentBufferSize = 256000;
+        }
+        public async Task<List<Lowongan>> GetAllData()
+        {
+            ListLowongan data = null;
+            var uri = new Uri(Pengaturan.BaseUrl);
+            try
+            {
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    data = JsonConvert.DeserializeObject<ListLowongan>(content);
+                }
+                return data.CONTENT;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
