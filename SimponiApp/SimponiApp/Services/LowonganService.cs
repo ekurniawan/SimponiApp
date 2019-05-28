e9d4c176c2a007986c483441f189668989ff6deb
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-28 12:33:58 +0700

    menampilkan lowongan

--- SimponiApp/SimponiApp/Services/LowonganService.cs
+++ SimponiApp/SimponiApp/Services/LowonganService.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SimponiApp.Helpers;
 using SimponiApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,14 +23,17 @@ namespace SimponiApp.Services
         public async Task<List<Lowongan>> GetAllData()
         {
             ListLowongan data = null;
-            var uri = new Uri(Pengaturan.BaseUrl);
+            var uri = new Uri("http://api.uajy.ac.id/apisimponi/api/lowongan");
             try
             {
                 var response = await _client.GetAsync(uri);
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
-                    data = JsonConvert.DeserializeObject<ListLowongan>(content);
+                    var cleaning = JsonConvert.DeserializeObject(content);
+                    JObject json = JObject.Parse(cleaning.ToString());
+
+                    data = JsonConvert.DeserializeObject<ListLowongan>(json.ToString());
                 }
                 return data.CONTENT;
             }
---
+++ SimponiApp/SimponiApp/Views/LowonganPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SimponiApp.Models;
+using SimponiApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimponiApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LowonganPage : ContentPage
+    {
+        private LowonganService _myService;
+        public List<Lowongan> ListLowongan { get; set; }
+        public LowonganPage()
+        {
+            _myService = new LowonganService();
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+                lvLowongan.ItemsSource = await _myService.GetAllData();
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Kesalahan", ex.Message, "OK");
+            }
+            
+        }
+    }
+}
