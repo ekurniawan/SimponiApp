2375cfedffdc50c0e23aeabf877ebb95ad335e8d
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-05-28 10:46:50 +0700

    menambahkan class Lowongan

---
+++ SimponiApp/SimponiApp/Models/Lowongan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimponiApp.Models
+{
+    public class Lowongan
+    {
+        public string ID_LOWONGAN_KERJA { get; set; }
+        public string PERUSAHAAN { get; set; }
+        public string TGL_SELESAI { get; set; }
+    }
+
+    public class ListLowongan
+    {
+        public string STATUS { get; set; }
+        public List<Lowongan> CONTENT { get; set; }
+        public string MESSAGE { get; set; }
+    }
+}
